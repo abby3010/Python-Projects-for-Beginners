120eed394d954a8c4d88dcac9cc0d89e307ad7e5
abby3010 <abhayubhale.30@gmail.com>
2020-10-11 19:44:21 +0530

    student management project added

---
+++ Student Management System/student_management_system.py
@@ -0,0 +1,6 @@
+# Q.2. Write a program to build a simple Student Management System using Python which can perform the following operations:
+# 1. Accept
+# 2. Display
+# 3. Search
+# 4. Delete
+# 5. Update
